dfcc14c654cdaf87dd7342664b00a8bb116462a7
srikaanth <srikaanth62@gmail.com>
2023-05-14 10:01:17 +0530

    terraform vars

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "Sridevi"
-  type  = "String"
-  value = "my name"
+  name  = "Srikaanth"
+  type  = "SecureString"
+  value = "secured"
+  key_id = "b4390df6-235e-4da9-a62d-1f713a82e28d"
 }
