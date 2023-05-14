3babb26ed918eba18b0ed34bc8bd8777de97f121
srikaanth <srikaanth62@gmail.com>
2023-05-14 10:25:36 +0530

    terraform vars

---
+++ env-dev/main.tfvars
@@ -0,0 +1,5 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.srikaanth62.online:8080/" },
+  { name = "dev.frontend.cart_url", value = "http://shipping-dev.srikaanth62.online:8080/" }
+
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name  = "Srikaanth"
-  type  = "SecureString"
-  value = "secured"
+resource "aws_ssm_parameter" "parameters" {
+  count = length (var.parameters)
+  name  = var.parameters[count.index].name
+  type  = "String"
+  value = var.parameters[count.index].value
   key_id = "b4390df6-235e-4da9-a62d-1f713a82e28d"
 }
